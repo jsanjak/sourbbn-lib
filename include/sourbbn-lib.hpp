eba58aa034168794b7de7efc6cab224375555fad
jsanjak <jsanjak@gryphonscientific.com>
2019-09-12 17:06:26 -0400

    init repo

---
+++ include/sourbbn-lib.hpp
@@ -0,0 +1,20 @@
+//
+// Created by jsanjak on 9/11/2019.
+//
+
+#ifndef TBD_MOBILE_SOURBBN_HPP
+#define TBD_MOBILE_SOURBBN_HPP
+
+#include <string>
+
+#endif //TBD_MOBILE_SOURBBN_HPP
+
+namespace sourbbn {
+
+inline std::string from_sourbbn(const std::string &s1 ) {
+
+    return s1 + " From: SourBBN";
+
+}
+
+}
