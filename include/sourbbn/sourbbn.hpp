3786fe80a7214221d1b5f545b5af7f27a9874f72
jsanjak <jsanjak@gryphonscientific.com>
2019-09-12 17:50:16 -0400

    refactor as shared lib

---
+++ include/sourbbn/sourbbn.hpp
@@ -0,0 +1,16 @@
+//
+// Created by jsanjak on 9/11/2019.
+//
+
+#ifndef TBD_MOBILE_SOURBBN_HPP
+#define TBD_MOBILE_SOURBBN_HPP
+
+#include <string>
+
+
+namespace sourbbn {
+
+std::string from_sourbbn(const std::string &s1);
+
+}
+#endif //TBD_MOBILE_SOURBBN_HPP
---
+++ src/sourbbn.cpp
@@ -0,0 +1,16 @@
+//
+// Created by jsanjak on 9/12/2019.
+//
+
+#include <string>
+#include "sourbbn/sourbbn.hpp"
+
+namespace sourbbn {
+
+    std::string from_sourbbn(const std::string &s1 ) {
+
+        return s1 + " From: SourBBN STATIC";
+
+    }
+
+}
